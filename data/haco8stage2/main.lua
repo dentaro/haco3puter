b3c1557bd9baac996a64238bc18aeccc22d910d7
dentaro <77978725+dentaro@users.noreply.github.com>
2024-03-21 13:20:33 +0900

    Add files via upload

--- data/haco8stage2/main.lua
+++ data/haco8stage2/main.lua
@@ -1,5 +1,4 @@
 cl = 0
-
 function get_map_sprn(sprx,spry)
   local celx=flr(sprx/8)
   local cely=flr(spry/8)
@@ -11,12 +10,12 @@ function get_map_flag(sprx,spry)
   local celx=flr(sprx/8)
   local cely=flr(spry/8)
   local celc=mget(celx,cely)
-  -- -- return celc
+  -- return celc
   return fget(celc,0)
 end
 
-
-function collision(x,y)
+function collition(x,y)
+  
   return get_map_flag(x-cl,y+2-cl) or get_map_flag(x+6-cl,y+2-cl) or get_map_flag(x-cl,y+7-cl) or get_map_flag(x+6-cl,y+7-cl)
 end
 
@@ -34,70 +33,76 @@ nf=2 -- アニメーションするフレーム数(足踏みは2フレーム)
 t=0
 
 function _init()
-  -- map(0,0,0,0,8,8,"/init/map/0.png")
+  -- ここに書いてもグローバル変数になるようにしたい
+  -- x=8
+  -- y=8
+  -- music(0);
 end
 
+
 function input()
-  -- if btn(6) >= 2 then
-  --   appmode(appinfo(0), 1)--エディットモードで開く
-  -- end
-  
   local prex = x
   local prey = y
   local pressed=false
-
-  if (btn(1)>=2) then
+  if (btn(1)>=1) then
    x = x-1
    d=1
    pressed=true
   end
-  if (btn(2)>=2) then
+  if (btn(2)>=1) then
     x = x+1
    d=2
    pressed=true
   end
-  if (btn(3)>=2) then
+  if (btn(3)>=1) then
    y = y-1
    d=3
    pressed=true
   end
-  if (btn(4)>=2) then
+  if (btn(4)>=1) then
    y = y+1
    d=4
    pressed=true
   end
-
   if pressed then
    s=d+flr((t%(nf*ipf))/ipf+1)*8 --8個先が次のコマ
   else
    s=d
   end
 
-  if collision(x,y) == true then
-    -- sound=8
+  if collition(x,y) == true then
     x=prex
     y=prey
   end
+
  end
 
 function _update()
+  
+  -- fset(5,0,0)--暗い草原スプライト5を通れるように（0ビットを0に）する
 
-  fset(1,0,1)--水スプライト1を通れるように（0ビットを1に）する
-
+  -- t = t+1
   input()
+
+
+  -- lefttop=get_map_flag(x+1-cl,y+2-cl)
+  -- righttop=get_map_flag(x+5-cl,y+2-cl)
+  -- leftbottom=get_map_flag(x+1-cl,y+7-cl)
+  -- rightbottom=get_map_flag(x+5-cl,y+7-cl)
 
 end
 
 function _draw()
+  
   cls(0)
-  map(0,0,0,0,8,8,"/init/map/0.png")
-  if x>46 and y>46 then go2("/haco8stage2/main.lua", 100) end--状態3で立ち上げる
+  map(0,0,0,0,8,8,"/init/map/2.png")
+  -- spr8(1,x,y,1,1,false,false)
 
-  spr8(9,48,48)--アイテム
+  if x>46 and y>46 then go2("/haco8stage1/main.lua") end
+  spr8(10,48,48)--アイテム
   spr8(s+64,x,y)
 
-  -- print(gstat(),68,2,16,0,7)--ゲーム状態を表示
-  print(gstat(),68,2,7)
+  print(gstat(),68,2,7)--ゲーム状態を表示
 
   -- pset(x+1-cl,y+2-cl, 8)
   -- pset(x+5-cl,y+2-cl, 8)
@@ -105,10 +110,5 @@ function _draw()
   -- pset(x+5-cl,y+7-cl, 8)
   -- print(lefttop.."/"..righttop,68,2,7)
   -- print(leftbottom.."/"..rightbottom,68,8,7)
-
-  -- rectfill(0,0,127,127,0)
- -- 前回再生されたSFX番号を画面に表示する
---  if last_sound>-1 then
-  -- print(last_sound, 68,14,7)
---  end
+  
 end
